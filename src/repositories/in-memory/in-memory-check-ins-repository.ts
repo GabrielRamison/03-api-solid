82e528b790938a85f9b6df261c98cef24ff88a0e
GabrielRamison <gabriel.ramison@gmail.com>
2024-08-13 14:18:36 -0300

    Caso de uso de criação de academia

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,7 +1,7 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { Prisma, CheckIn } from '@prisma/client'
-import { CheckInsRepository } from '../check-ins-repository'
-import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
@@ -30,11 +30,9 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       user_id: data.user_id,
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
-      create_at: new Date(),
+      created_at: new Date(),
     }
-
     this.items.push(checkIn)
-
     return checkIn
   }
 }
--- src/use-cases/get-user-profile.spec.ts
+++ src/use-cases/get-user-profile.spec.ts
@@ -2,7 +2,7 @@ import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
 import { GetUserProfileUseCase } from './get-user-profile'
-import { ResourceNotfoundError } from './errors/resource-not-found-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let usersRepository: InMemoryUsersRepository
 let sut: GetUserProfileUseCase
@@ -32,6 +32,6 @@ describe('Get User Profile Use case', () => {
       sut.execute({
         userId: 'non-existing-id',
       }),
-    ).rejects.toBeInstanceOf(ResourceNotfoundError)
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
